5b3e4c6c730c9e9abdb6590a116b9f7e73a95edc
alexciru <alejandrocirugeda@gmail.com>
2025-08-19 21:44:34 +0200

    clean the code and added information about Teltonika and Vlands

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from docx.shared import Inches
 from assets_interface import AtlassianAssetsAPI
 import os
 from dotenv import load_dotenv
+import tqdm
 
 def replace_text_in_paragraphs(paragraphs, replacements):
     """
@@ -101,21 +102,29 @@ def get_pib_information(pib_number):
 ################################################################################
 
 if __name__ == "__main__":
-    load_dotenv(".env")  # Load environment variables from .env file
+    # Define the variables for the executino
     template_path = "PiB Vendor guide T560 - ALEX_template.docx"
     output_path = "output.docx"
     PiB_Number = "138"
-    # Example usage
+    
 
-
+    # Initialize the Atlassian Assets API
+    # All the secret keys are store in the .env file on the same directory
+    # change the åath to .env if need
+    load_dotenv(".env")  
     api = AtlassianAssetsAPI(
-        email=os.getenv("ATLANTSIA_EMAIL"),     # Replace with your Atlassian email
-        api_token=os.getenv("ATLANTSIA_API_TOKEN"),  # Replace with your Atlassian API token
-        domain=os.getenv("ATLANTSIA_DOMAIN"),  # Replace with your Atlassian domain
+        email=os.getenv("ATLANTSIA_EMAIL"),    
+        api_token=os.getenv("ATLANTSIA_API_TOKEN"),  
+        domain=os.getenv("ATLANTSIA_DOMAIN"),  
     )
 
+    # Needed to initialize the API and get the workspace ID
     api.get_workspace_id()
-    # GET ALL THE PIBS
+
+    # Step 1: Retrieve all object IDs of type 40 (PiB)
+    # This will return a dictionary with object IDs as keys and object data as values
+    # we need to filter the object IDs to find the one that matches the PiB number
+    # TODO: Improve this to use a more efficient search to avoid multiple REST API calls
     object_dicc = api.get_object_ids("objectTypeId = 40")
     # get the pib number from the object id
     for id in object_dicc.keys():
@@ -125,46 +134,70 @@ if __name__ == "__main__":
             break
 
     pib_data = api.get_object(object_id)
+    pib_dic = api.attributes_to_dict(pib_data)
     
     print(f"Object ID: {pib_data['id']}, Name: {pib_data['name']}")
     
 
+    print("-------")
+    print("-------")
+    print("-------")
+    print("-------")
 
-    obj_dic = api.attributes_to_dict(pib_data)
-    teltonika_dicc = obj_dic["Teltonika"][0]
+ 
+    vm_dicc_list = pib_dic["Virtual Machines"]
+    print("Virtual Machines:")
+    for vm_dicc in vm_dicc_list:
+        # TODO add values: name, ip+address, hostname, host memory
+        attributes = api.get_object_attributes(id)
+        print(vm_dicc['displayValue'] + " - " + vm_dicc['referencedObject']['id'])
+        
+    
+    # Retrieve the Teltonika object that is linked to the PIB
+    # We need to obtain the referenced object ID from the PIB attributes and obtain all the values
+    # info needed: Model, WiFi SSID, WiFi Password
+    print("-------")
+    print("-------")
+    print("-------")
+    print("-------")
+    teltonika_dicc = pib_dic["Teltonika"][0]
     teltonika_id = teltonika_dicc['referencedObject']['id']
-    print(f"Teltonika reference: {teltonika_dicc['referencedObject']}")
-
     teltonika_obj = api.get_object(teltonika_id)
     teltonika_attr = api.attributes_to_dict(teltonika_obj)
-    print("-------")
-    print("-------")
-    print("-------")
-    print("-------")
-    #print(teltonika_attr["WiFi SSID"])
-    #print(teltonika_attr["Status"])
-    #print(teltonika_attr["WiFi Password"])
     
     print(print(list(teltonika_attr.keys())))
     print(teltonika_attr["Model"][0]["displayValue"])
     print(teltonika_attr["WiFi SSID"][0]["displayValue"])
     print(teltonika_attr["WiFi Password"][0]["displayValue"])
 
-    #print(f"Teltonika dictionary: {teltonika_dicc}")
-    #print(f"Teltonika: {teltonika_dicc['id']}")
-    #print(f"Teltonika: {teltonika_dicc['model']}")
-    #print(f"Teltonika: {teltonika_dicc['WiFi SSID']}")
-    #print(f"Teltonika: {teltonika_dicc['WiFi Password']}")
+
     
-
-
-    # Retrieved 
-
     # Retrieve its attributes
     pib_atrb = api.get_object_attributes(object_id)
     teltonika_dicc = api.get_object_attributes(teltonika_id)
-    print(teltonika_dicc)
     
+
+
+    # Retrieve the VLANs object
+    # This information is not linked to the PIB, so we will not retrieve it here
+    # We need to retrieve the all the objects of type 34 (VLANs)
+    # and keep the ones that match the Location 
+    # info needed: VLAN ID, VLAN Name, VLAN IP Range, Gateway, subMask
+    print("-------")
+    print("-------")
+    vlans_dicc = api.get_object_ids("objectTypeId = 34")
+    # pint the number of VLANs found
+    print(f"Found {len(vlans_dicc)} VLANs")
+    print("VLANs:")
+    for vlan_id, vlan_data in vlans_dicc.items():
+        vlan_attributes = api.get_object_attributes(vlan_id)
+        if "Site" in vlan_attributes:
+            if vlan_attributes['Site'] == pib_atrb["Location"]:
+                print(f"VLAN ID: {vlan_attributes['ID']}, Name: {vlan_attributes['Description']} - {vlan_attributes['IP Range']} - {vlan_attributes['Gateway']}")
+
+
+
+
     # Retrieve its attributes
     replacements = {
         "<PIB_NUMBER>": PiB_Number,
@@ -174,7 +207,9 @@ if __name__ == "__main__":
         "<TELTONIKA>": pib_atrb["Teltonika"],
         "<MODEL>": teltonika_dicc["Model"],
         "<WIFI_SSID>": teltonika_dicc["WiFi SSID"],
-        "<WIFI_PASSWORD>": teltonika_dicc["WiFi Password"]
+        "<WIFI_PASSWORD>": teltonika_dicc["WiFi Password"],
+        "<CLIENT_DOMAIN>": "xxxxxxxxxxxxxxx",   # TODO: WHERE TO GET THIS?
+        "<CLIENT_USER>": "xxxxxxxxxxxxxxx",     # TODO: WHERE TO GET THIS?
     }
 
     image_replacements = {
